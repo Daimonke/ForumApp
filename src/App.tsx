8707a1b5fe3975846ce3926d9bb3a25206644ac9
Daimonke <Daimonas98@gmail.com>
2022-07-17 13:21:56 +0300

    Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Context from "./context/Context";
 import Index from "./layout/Index";
 
 function App() {
   return (
     <Context>
-      {/* HashRouter for Github-pages and package.json "proxy" bug */}
-      <HashRouter>
+      <BrowserRouter>
         <Index />
-      </HashRouter>
+      </BrowserRouter>
     </Context>
   );
 }
